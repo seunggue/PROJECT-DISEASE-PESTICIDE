9e7f3aa7c67917171deac0884bf25f0f82b2c13c
multicampus <roadvirushn@gmail.com>
2020-06-03 13:10:04 +0900

    S02P31C102-72 [+] TEST(backend): flask sever test

--- front/lib/screen/screen_map.dart
+++ front/lib/screen/screen_map.dart
@@ -6,39 +6,35 @@ import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:http/http.dart';
 
-class MapScreen extends StatefulWidget{
-
+class MapScreen extends StatefulWidget {
   @override
   _MapScreenState createState() => _MapScreenState();
 }
 
-
 class _MapScreenState extends State<MapScreen> {
-//  final _webViewController = WebViewController();
-//  WebViewController _webViewController;
+  WebviewScaffold webview = WebviewScaffold(
+    url: 'http://7e97851564ca.ngrok.io/map',
+    geolocationEnabled: true,
+//    withZoom: true,
+    useWideViewPort: true,
+//    displayZoomControls: true,
+    withOverviewMode: true,
+    resizeToAvoidBottomInset: true,
+  );
+  final flutterWebviewPlugin = new FlutterWebviewPlugin();
+  flutterWebviewPlugin.onStateChanged.listen((viewState) async {
+    if (viewState.type == WebViewState.finish)
+})
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
 //    Size screenSize = MediaQuery.of(context).size;
 //    double width = screenSize.width;
 //    double height = screenSize.height;
-    return SafeArea(child: Scaffold(
-      appBar: AppBar(
-        title: Text('맵 뷰'),
-      ),
-      body:
-        WebviewScaffold(
-          url: 'http://3f465fa16d44.ngrok.io/map',
-//      WebView(
-//        userAgent: "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Mobile Safari/537.36",
-//        initialUrl: '',
-//        javascriptMode: JavascriptMode.unrestricted,
-//        onWebViewCreated: (WebViewController webViewController){
-//          _webViewController = webViewController;
-//          _loadHtmlFromAssets();
-//        },
-      )
-    ));
-
+    return SafeArea(
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text('맵 뷰'),
+            ),
+            body: webview));
   }
-
 }
